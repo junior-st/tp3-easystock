03f7a6280dc2eae11bc6ce47d7208926514cacfa
Junior Chakoutio <chakoutiojunior@gmail.com>
2026-01-10 02:10:19 +0100

    10/01/2026

--- commande.cpp
+++ commande.cpp
@@ -1,1 +1,30 @@
 #include "commande.h"
+
+Commande::Commande(Client& client, std::vector<Produit> panier_commande)
+    : proprietaire_commande_(client),
+      panier_commande_(panier_commande)
+{
+}
+
+Client Commande::getProprietaireCommande() const
+{
+    return proprietaire_commande_;
+}
+
+std::vector<Produit> Commande::getPanierCommande() const
+{
+    return panier_commande_;
+}
+
+std::string Commande::getEtatLivraison() const
+{
+    return etat_;
+}
+
+std::ostream& operator<<(std::ostream& os, const Commande& com)
+{
+    os << com.getProprietaireCommande().getNom() << " " << com.getProprietaireCommande().getPrenom() 
+       << com.getProprietaireCommande().afficherProduitPanier()<<std::endl<<com.getEtatLivraison();
+
+    return os;
+}
--- commande.h
+++ commande.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "client.h"
+#include "produit.h"
 class Commande
 {
+private:
+	Client proprietaire_commande_;
+	std::vector<Produit> panier_commande_;
+	std::string etat_ = "pas liver";
+public:
+	Commande(Client& client,std::vector<Produit> panier_commande = {});
+	Client getProprietaireCommande()const;
+	std::vector<Produit> getPanierCommande()const;
+	std::string getEtatLivraison()const;
 };
+std::ostream& operator << (std::ostream& os, const Commande& Com);
 
